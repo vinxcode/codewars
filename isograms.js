dc402ed1f2311de1f1018bd8f16fa51c042bcc4f
Kevin <vincitbussiness@gmail.com>
2024-10-08 11:08:11 -0600

    isograms finalized

--- isograms.js
+++ isograms.js
@@ -1,19 +1,30 @@
 function isIsogram(str) {
 
+    str = str.toLowerCase()
+
     let firstIndex
     let lastIndex
+    let result = false
 
     for (i = 0; i < str.length; i++) {
         lastIndex = str.lastIndexOf(str.charAt(i))
         firstIndex = str.indexOf(str.charAt(i))
 
         if (firstIndex !== lastIndex) {
-            return true
+            result = false
+            break;
         } else {
-            return false
+            result = true
         }
 
     }
 
-    return true
+    return str === "" ? true : result
 }
+
+isIsogram('Dermatoglyphics')
+isIsogram('isogram')
+isIsogram('aba')
+isIsogram('moOse')
+isIsogram('isIsogram')
+isIsogram('')
